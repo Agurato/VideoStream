ba3b84d7bdb89faa9248220c531a3ad1b0d6a44e
nowanda92 <antoine.regnier92@gmail.com>
2017-10-24 18:28:24 +0200

    ajout de la vue setting

---
+++ app/src/main/java/fr/vmonot/videostream/VideoStreamSettingsActivity.java
@@ -0,0 +1,13 @@
+package fr.vmonot.videostream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoStreamSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream_settings);
+    }
+}
